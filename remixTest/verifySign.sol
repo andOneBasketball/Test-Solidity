8886682bce8fe7042769c15091d6e0b26877f2ac
andOneBasketball <hhhhk00791@gmail>
2024-09-26 21:12:57 +0800

    增加注解

--- remixTest/verifySign.sol
+++ remixTest/verifySign.sol
@@ -9,7 +9,7 @@ pragma solidity 0.8.27;
 
 ethereum.enable()
 account = "0x5639Bc2D96c7bA37EECA625599B183241A2bBE6c"
-hash = "0x1bf2c0ce4546651a1a2feb457b39d891a6b83931cc2454434f39961345ac378c"   //(getMessageHash的返回值)
+hash = "0x1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8"   //(getMessageHash传入 hello 的返回值)
 ethereum.request({method: "personal_sign", params: [account, hash]})
 
 编写一个智能合约，实现以下功能：
@@ -43,6 +43,8 @@ contract VerifySig {
 
     function _split(bytes memory _sig) internal pure returns(bytes32 r, bytes32 s, uint8 v){
         require(_sig.length == 65, "invalid signature length");
+        // 字节数组在内存中的布局是前32字节用来存储字节数组的长度，后面跟着的字节数组内容。当作为函数的返回值时，EVM会自动略过前32字节的长度信息，直接返回后面的字节数组内容。
+        // 所以，这里的_sig[0:32]实际上是长度信息，可以直接忽略。
         assembly {
             r := mload(add(_sig, 32))
             s := mload(add(_sig, 64))
