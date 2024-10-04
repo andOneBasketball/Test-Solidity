cf54bb1e43d9a7991043080c9670cf213a64e595
andOneBasketball <hhhhk00791@gmail>
2024-10-04 21:38:13 +0800

    revert的验证

---
+++ remixTest/revertCase.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+contract revertCase {
+    uint public count = 0;
+
+    error MyError(string message);
+
+    function revertTest() public {
+        count += 1;
+        revert MyError("This is a revert test");
+    }
+
+    function transferTest(address payable _to, uint _amount) public {
+        count += 1;
+        _to.transfer(_amount);
+    }
+}
